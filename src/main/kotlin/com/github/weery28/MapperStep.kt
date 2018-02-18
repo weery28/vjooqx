7830930d027a522a0e6badbdceab3c793ab4086e
Oleg Ryazanov <rou.weery@gmail.com>
2018-02-18 13:13:54 +0300

    Implemented aliases mapping

--- src/main/kotlin/com/github/weery28/MapperStep.kt
+++ src/main/kotlin/com/github/weery28/MapperStep.kt
@@ -1,5 +1,7 @@
 package com.github.weery28
 
+import com.fasterxml.jackson.databind.JsonNode
+import com.fasterxml.jackson.databind.ObjectMapper
 import com.github.weery28.exceptions.FlatMappingException
 import com.github.weery28.json.JsonParser
 import io.reactivex.Single
@@ -16,7 +18,10 @@ class MapperStep(
     fun <T> to(pClass: Class<T>): Single<T?> {
 
         return resultSingle.flatMap {
-            val jsonResult = it.rows.firstOrNull()?.encode()
+            val jsonResult = it.rows.firstOrNull()?.let {
+                return@let unpackAlias(it).encode()
+            }
+
             if (jsonResult != null) {
                 Single.just((jsonParser.encode(jsonResult, pClass)))
             } else {
@@ -55,10 +60,9 @@ class MapperStep(
                             resultJson.put(key, JsonArray(mutableListOf(jsonField.value)))
                         } else {
                             val array = resultJson.getJsonArray(key)
-                            if (jsonField.value == null ){
+                            if (jsonField.value == null) {
                                 array.addNull()
-                            }
-                            else{
+                            } else {
                                 array.add(jsonField.value)
                             }
                         }
@@ -66,6 +70,54 @@ class MapperStep(
                 }
             }
             return@map jsonParser.encode(resultJson.encode(), pClass)
+        }
+    }
+
+    private fun unpackAlias(jsonResult: JsonObject): JsonObject {
+
+        val unpackingResult = JsonObject()
+
+        jsonResult.forEach {
+            val splitKeys = it.key.split(".")
+            val topLevelKey = splitKeys[0]
+            if (splitKeys.size != 1) {
+                val currentNode = unpackingResult.getJsonObject(topLevelKey)
+                val node = getNode(splitKeys.toMutableList(), it.value, currentNode?.let {
+                    return@let JsonObject().put(topLevelKey, it)
+                })
+                unpackingResult.put(topLevelKey, node.getValue(topLevelKey))
+
+            } else {
+                unpackingResult.put(topLevelKey, it.value)
+            }
+        }
+        System.out.print(unpackingResult.encode())
+        return unpackingResult
+    }
+
+    private fun getNode(path: MutableList<String>, value: Any?, node: JsonObject? = null): JsonObject {
+        if (path.size != 1) {
+            if (node == null) {
+                return JsonObject().put(path.removeAt(0), getNode(path, value))
+            } else {
+
+                val key = path.removeAt(0)
+                if (node.containsKey(key)) {
+                    return node.copy()
+                            .put(key, getNode(path, value, node.getJsonObject(key)))
+                } else {
+                    return node
+                            .copy()
+                            .put(key, getNode(path, value, null))
+                }
+            }
+        } else {
+            if (node == null) {
+                return JsonObject().put(path.first(), value)
+            } else {
+                return node.put(path.first(), value)
+            }
+
         }
     }
 }
